cd29a533b99aed1c167495f337f10150ba7a4ec6
ValentinAlexandrovGeorgiev <valentin.georgiev95@abv.bg>
2015-10-29 23:40:55 +0200

    crypt password with Crypto-js

--- public/js/data.js
+++ public/js/data.js
@@ -3,10 +3,16 @@ var data = function(){
     function register(user){
         var promise = new Promise(function(resolve, reject){
             var url = 'api/users';
+
+            var reqUser = {
+                username: user.username,
+                passhash: CryptoJS.SHA1(user.password).toString()
+            };
+
             $.ajax(url,{
                 method: 'POST',
                 contentType: 'application/json',
-                data: JSON.stringify(user),
+                data: JSON.stringify(reqUser),
                 success: function(res){
                     resolve(res);
                 }
